b7adfa3ed15149053854cfd0a8605e590e47115d
Mukaffi28 <mukaffi28@gmail.com>
2021-09-10 00:56:08 +0600

    Firebase add

---
+++ app/src/main/java/com/Rijeet_Mukaffi/todolist/LoginPage.java
@@ -0,0 +1,15 @@
+package com.Rijeet_Mukaffi.todolist;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
