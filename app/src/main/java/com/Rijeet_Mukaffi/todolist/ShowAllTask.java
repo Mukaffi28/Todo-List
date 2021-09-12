dcfa2e6c4000edc0f9c9bcb9a529e1a0baf8ff0b
Mukaffi28 <mukaffi28@gmail.com>
2021-09-12 15:56:39 +0600

    new file added

---
+++ app/src/main/java/com/Rijeet_Mukaffi/todolist/ShowAllTask.java
@@ -0,0 +1,14 @@
+package com.Rijeet_Mukaffi.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowAllTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_all_task);
+    }
+}
---
+++ app/src/main/java/com/Rijeet_Mukaffi/todolist/Taskadd.java
@@ -0,0 +1,14 @@
+package com.Rijeet_Mukaffi.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Taskadd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taskadd);
+    }
+}
